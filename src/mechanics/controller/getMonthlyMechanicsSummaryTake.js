35da225b36d9a91932a94a5f0d93cac5e07745cf
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2018-11-12 15:07:56 +0100

    monthly raport

--- src/mechanics/controller/getMonthlyMechanicsSummaryTake.js
+++ src/mechanics/controller/getMonthlyMechanicsSummaryTake.js
@@ -4,7 +4,7 @@ const repository = require('./../repository/mechanicsRepository');
 
 async function getMonthlyMechanicsSummaryTake(req, res) {
     try {
-        const data = await repository.getMonthlyMechanicsSummaryTake();
+        const data = await repository.getMonthlyMechanicsSummaryTake(req.params.year, req.params.month, req.params.branch);
         return res.json(data.recordset);
     } catch (err) {
         console.error(err);
--- src/mechanics/repository/mechanicsRepository.js
+++ src/mechanics/repository/mechanicsRepository.js
@@ -8,17 +8,34 @@ const mssql = require('./../../shared/mssql');
  * @returns {Promise<*|void>}
  */
 function getDailyMechanicsSummaryTake(date, branch) {
-    return mssql.exec(`SELECT DISTINCT zl_id, zlp_mechanik, zl_wartoscUslugiN
-        FROM [AcomNEX].[dbo].[nex_wyp_SerZlecenia]
-        JOIN [AcomNEX].[dbo].[nex_wyp_SerZleceniaPozycje]
-        ON [AcomNEX].[dbo].[nex_wyp_SerZlecenia].[zl_id]=[AcomNEX].[dbo].[nex_wyp_SerZleceniaPozycje].[zlp_zlid]
-        WHERE zl_dataZamkniecia = '${date}'
-        AND zl_nrroz = '${branch}'
-        AND zlp_mechanik != '.......... ..........'`);
+    return mssql.exec(`SELECT zlp_mechanik, sum(total) AS total
+        FROM (
+            SELECT DISTINCT zl_id, zlp_mechanik, zl_wartoscUslugiN AS total
+              FROM [AcomNEX].[dbo].[nex_wyp_SerZlecenia]
+              JOIN [AcomNEX].[dbo].[nex_wyp_SerZleceniaPozycje] 
+                ON [AcomNEX].[dbo].[nex_wyp_SerZlecenia].[zl_id]=[AcomNEX].[dbo].[nex_wyp_SerZleceniaPozycje].[zlp_zlid]
+              WHERE zl_dataZamkniecia = '${date}'
+              AND zl_nrroz = '${branch}'
+              AND zlp_mechanik != '.......... ..........'
+              AND zlp_mechanik != ' ..........  ..........'
+            ) x
+        GROUP BY zlp_mechanik`);
 }
 
-function getMonthlyMechanicsSummaryTake(date, branch) {
-    return null
+function getMonthlyMechanicsSummaryTake(year, month, branch) {
+    return mssql.exec(`SELECT zlp_mechanik, sum(total) AS total
+        FROM (
+            SELECT DISTINCT zl_id, zlp_mechanik, zl_wartoscUslugiN AS total
+                FROM [AcomNEX].[dbo].[nex_wyp_SerZlecenia]
+                JOIN [AcomNEX].[dbo].[nex_wyp_SerZleceniaPozycje]
+                ON [AcomNEX].[dbo].[nex_wyp_SerZlecenia].[zl_id]=[AcomNEX].[dbo].[nex_wyp_SerZleceniaPozycje].[zlp_zlid]
+                WHERE zl_mc = '${month}'
+                AND zl_rok = ${year}
+                AND zl_nrroz = '${branch}'
+                AND zlp_mechanik != '.......... ..........'
+                AND zlp_mechanik != ' ..........  ..........'
+            ) x
+        GROUP BY zlp_mechanik`);
 }
 
 module.exports = {
